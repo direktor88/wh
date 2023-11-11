b942d87b04e82fcb9aae8d3423be2cd06e97a20d
Dmitri <115719914+direktor88@users.noreply.github.com>
2023-11-11 22:17:42 +0500

    тестирую поле

--- main.py
+++ main.py
@@ -6,23 +6,41 @@ class Field():
         self.field = field
         self.hid = hid
         self.field = [[" "] * 6 for i in range(6)]
-    #     print("    | 1 | 2 | 3 | 4 | 5 | 6 | ")
-    # print("  --------------------------- ")
-    # for i, row in enumerate(field):
-    #     row_str = f"  {i + 1} | {' | '.join(row)} | "
-    #     print(row_str)
-    #     print("  --------------------------- ")
+
+    def __str__(self):
+
+    res = "    | 1 | 2 | 3 | 4 | 5 | 6 | "
+    for i, row in enumerate(self.field):
+        res += f"\n{i + 1} | " + " | ".join(row) + " | "
+    if self.hid:
+        res = res.replace("■", "O")
+
+    def add_ship(self):
 
 
-class Dot:
+class Dot:  # coordinates
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
 
 class Ship:
-    def __init__(self, smallship) -> None:
+    def __init__(self, smallship, v, h, health=1) -> None:
+        self.smallship = smallship  # one "dot"
         self.SetPos(smallship)
+        self.h = h  # horizontal
+        self.v = v  # vertical
+        self.setcolor(0)
+        self.health = health
+
+    def setColor(self, color):
+        self.color = color
+
+    def getColor(self):
+        return self.color
 
     def setPos(self, smallship):
         self.smollship = smallship
@@ -33,24 +51,25 @@ class Ship:
     def isIn(self, x, y) -> bool:
         return False
 
+
 class Fleet(Ship):
-    def __init__(self, pos, w, h):
+    def __init__(self, pos, w, h):  # позиция, ширина, длинна корабля
         super().__init__(pos)
         self.w = w
         self.h = h
-    def getWidth(self):
-        return self.w
+        self.w = 1
+        # def getWidth(self):
+
+    #     return self.w
 
     def getHeight(self):
         return self.h
 
-    def isIn(self, x , y):
+    def isIn(self, x, y):
         _pos = super().getPos()
-        if (_pos.x < x) and ((_pos.x + self.w)>x ) and (_pos.y < y) and ((_pos.y + self.h) > y):
+        if (_pos.x < x) and ((_pos.x + self.w) > x) and (_pos.y < y) and ((_pos.y + self.h) > y):
             return True
         return False
-
-
 
 
 class TwoShip(Ship):
@@ -63,5 +82,3 @@ class Game_gen:
 
 class User:
     pass
-
-
--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-class Field:
-    def __init__(self, hid=False, size):
-        self.size = size
-        self.hid = hid
-        self.field = [[" "] * 6 for _ in range(6)]
+from main import Field
 
-    def add_ship (self, )
-        print()
-        print("    | 1 | 2 | 3 | 4 | 5 | 6 | ")
-        print("  --------------------------- ")
-        for i, row in enumerate(field):
-            row_str = f"  {i + 1} | {' | '.join(row)} | "
-            print(row_str)
-            print("  --------------------------- ")
